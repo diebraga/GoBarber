b6d113a7b0b18526c69b11cc4ec03b5fae05fae0
unknown <diebraga0055@gmail.com>
2020-02-19 15:53:40 +0000

    added multer config upload files

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,38 +1,35 @@
-// 1. validation if use exist and psswrd ! matches
-// 2. security token
-
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
-import User from '../models/user';
 import authConfig from '../../config/auth';
+import User from '../models/user';
 
 class SessionController {
   async store(req, res) {
-    //valid schema
     const schema = Yup.object().shape({
+      name: Yup.string(),
       email: Yup.string()
         .email()
         .required(),
       password: Yup.string().required(),
     });
 
-    // pass validation = true or false
     if (!(await schema.isValid(req.body))) {
-    return res.status(400).json({ error: 'Validation fails' });
+      return res.status(400).json({ error: 'Validation fails' });
+    }
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
-
+    const user = await User.findOne({
+      where: { email },
+    });
     if (!user) {
-      return res.status(401).json({ error: 'Not found' });
+      return res.status(401).json({ error: 'User not found' });
     }
-    // 1.
+
     if (!(await user.checkPassword(password))) {
       return res.status(401).json({ error: "Dosn't match" });
     }
-
     const { id, name } = user;
 
     return res.json({
@@ -41,7 +38,6 @@ class SessionController {
         name,
         email,
       },
-      // 2.
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@
 // import midleware authentication
 
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -9,6 +11,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store); // creation user
 routes.post('/sessions', SessionController.store); // create session
@@ -16,5 +19,9 @@ routes.post('/sessions', SessionController.store); // create session
 routes.use(authMiddleware); // middlewares for validatioin
 
 routes.put('/users', UserController.update); // update user
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 export default routes;
