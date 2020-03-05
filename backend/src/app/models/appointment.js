9c6740ce2b2075f3e79dfa412b23598060d91f2f
unknown <diebraga0055@gmail.com>
2020-03-05 18:03:14 +0000

    added vituals to show frontend ifg appointment is cancelable and past

--- backend/src/app/models/appointment.js
+++ backend/src/app/models/appointment.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -6,6 +7,20 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        // add in frontend is past
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        // add frontend if is cancelable
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+          },
+        },
       },
       {
         sequelize,
