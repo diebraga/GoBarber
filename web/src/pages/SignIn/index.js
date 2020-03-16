b2214af42409ae2af9086670501ca9dfdc343047
unknown <diebraga0055@gmail.com>
2020-03-16 15:44:35 +0000

    added form and default layout style

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '../../assets/logo.svg';
 
 function SignIn() {
-  return <h1>login</h1>;
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form>
+        <Input name="email" type="email" placeholder="Your Email" />
+        <Input name="password" type="password" placeholder="Your Password" />
+
+        <button type="submit">Login</button>
+        <Link to="/SignUp">Create Account</Link>
+      </Form>
+    </>
+  );
 }
 
 export default SignIn;
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function SignUp() {
-  return <div />;
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '../../assets/logo.svg';
+
+function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="Gobarber" />
+
+      <Form>
+        <Input type="text" placeholder="Name and Surname" />
+        <Input type="email" placeholder="Your Email" />
+        <Input type="password" placeholder="Your Password" />
+
+        <button type="submit">Create Account</button>
+        <Link to="/SignUp">I already have an account</Link>
+      </Form>
+    </>
+  );
 }
 
-export default SignUp;
+export default SignIn;
