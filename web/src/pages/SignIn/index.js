a98ae7efeaaa5e29142d12a5ec547ac95afbb00f
unknown <diebraga0055@gmail.com>
2020-03-16 15:54:12 +0000

    save data form

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -6,11 +6,15 @@ import { Form, Input } from '@rocketseat/unform';
 import logo from '../../assets/logo.svg';
 
 function SignIn() {
+  function handleSubnmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Gobarber" />
 
-      <Form>
+      <Form onSubmit={handleSubnmit}>
         <Input name="email" type="email" placeholder="Your Email" />
         <Input name="password" type="password" placeholder="Your Password" />
 
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -6,14 +6,18 @@ import { Form, Input } from '@rocketseat/unform';
 import logo from '../../assets/logo.svg';
 
 function SignIn() {
+  function handleSubnmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Gobarber" />
 
-      <Form>
-        <Input type="text" placeholder="Name and Surname" />
-        <Input type="email" placeholder="Your Email" />
-        <Input type="password" placeholder="Your Password" />
+      <Form onSubmit={handleSubnmit}>
+        <Input name="text" type="text" placeholder="Name and Surname" />
+        <Input name="email" type="email" placeholder="Your Email" />
+        <Input name="password" type="password" placeholder="Your Password" />
 
         <button type="submit">Create Account</button>
         <Link to="/SignUp">I already have an account</Link>
