89f51a2e4e5dbfee73f549ca1552d1492f3f2bce
unknown <diebraga0055@gmail.com>
2020-03-23 15:40:09 +0000

    added component header + styles

---
+++ web/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo2 from '../assets/logo2.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo2} alt="" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Diego Braga</strong>
+              <Link to="/profile">My Profile</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Diego Braga"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
