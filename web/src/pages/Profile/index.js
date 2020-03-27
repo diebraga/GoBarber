9a26f3ff275c6f7a30c7c03e57c887e80b1a8f79
unknown <diebraga0055@gmail.com>
2020-03-27 15:30:32 +0000

    styles and profile form finished

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-function Profile() {
-  return <div />;
+import { Container, Logout } from './styles';
+
+export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your name" />
+        <Input name="email" type="email" placeholder="Your address" />
+        <hr />
+
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password"
+        />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+      <Logout>
+        <button type="button">Log Out</button>
+      </Logout>
+    </Container>
+  );
 }
-
-export default Profile;
