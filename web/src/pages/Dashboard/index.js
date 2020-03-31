fdbb41820e2217b6722d18667b4339dd7265ac21
unknown <diebraga0055@gmail.com>
2020-03-31 16:25:27 +0100

    styles struture dashboarde defined

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
-function Dashboard() {
-  return <h1>Dashboards</h1>;
-}
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-export default Dashboard;
+import { Container, Time } from './styles';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 of March</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>09:00</strong>
+          <span>Diego Braga</span>
+        </Time>
+        <Time avalible>
+          <strong>10:00</strong>
+          <span>Avalible</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Diego Braga</span>
+        </Time>
+        <Time>
+          <strong>12:00</strong>
+          <span>Diego Braga</span>
+        </Time>
+      </ul>
+    </Container>
+  );
+}
