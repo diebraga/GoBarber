b5c0498874458f6ca501bd5af2911d2d53e42725
unknown <diebraga0055@gmail.com>
2020-04-17 19:36:04 +0100

    added function book

--- mobile/gobarber/src/components/Appointent/index.js
+++ mobile/gobarber/src/components/Appointent/index.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import {parseISO, formatRelative} from 'date-fns';
 import {TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 
-import {Container, Left, Avatar, Info, Name, Time} from './styles';
+import {Container, Left, Avatar, Info, Name, Time, Canceled} from './styles';
 
-export default function Appointent() {
+export default function Appointment({data, onCancel}) {
+  const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date());
+  }, [data.date]);
+
   return (
-    <Container>
+    <Container past={data.past}>
       <Left>
-        <Avatar />
-
+        <Avatar
+          source={{
+            uri: data.provider.avatar
+              ? data.provider.avatar.url
+              : `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
+          }}
+        />
         <Info>
-          <Name>Diego Braga</Name>
-          <Time>In 3 Hours</Time>
+          <Name>{data.provider.name}</Name>
+          <Time>{dateParsed}</Time>
+          {data.canceled_at && <Canceled>Cancelled</Canceled>}
         </Info>
       </Left>
-
-      <TouchableOpacity onPress={() => {}}>
-        <Icon name="event-busy" size={20} color="#f64c75" />
-      </TouchableOpacity>
+      {data.cancellable && !data.canceled_at && (
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="event-busy" size={20} color="#f64c75" />
+        </TouchableOpacity>
+      )}
     </Container>
   );
 }
+
+Appointment.propTypes = {
+  data: PropTypes.shape({
+    date: PropTypes.string.isRequired,
+    canceled_at: PropTypes.string,
+    past: PropTypes.bool,
+    cancellable: PropTypes.bool,
+    provider: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
--- mobile/gobarber/src/pages/Dashboard/index.js
+++ mobile/gobarber/src/pages/Dashboard/index.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from '../../services/api';
 
 import {Container, Title, List} from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/background';
 import Appointment from '../../components/Appointent';
-const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
+  const [appointments, seAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      seAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    seAppointments.map((appointment) =>
+      appointment.id === id
+        ? {
+            ...appointment,
+            canceled_at: response.data.canceled_at,
+          }
+        : appointment,
+    );
+  }
+
   return (
     <Background>
       <Container>
         <Title>Appointments</Title>
 
         <List
-          data={data}
-          KeyExtrator={(item) => String(item)}
-          renderItem={({item}) => <Appointment data={item} />}
+          data={appointments}
+          KeyExtrator={(item) => String(item.id)}
+          renderItem={({item}) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </Background>
